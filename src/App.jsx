0069e70da9b81a483398675eb6d62a8426a16cdd
h.hajmirbaba <h.hajmirbaba@gmail.com>
2024-07-08 15:48:45 +0000

    anim push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import React, { useEffect
+, useRef } from 'react';
+import './App.css'; 
 import Header from "./components/Header.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
@@ -6,14 +9,32 @@ import Projects from "./components/Projects.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const componentRefs = useRef([]);
+
+  useEffect(() => {
+    componentRefs.current.forEach((ref, index) => {
+      setTimeout(() => {
+        if (ref.current) {
+          ref.current.classList.add('slide-in');
+        }
+      }, index * 300); // Adjust 300 for delay between components (in milliseconds)
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="components">
-          <About />
-          <Projects />
-          <Contact />
+          <div ref={(el) => componentRefs.current.push(el)}>
+            <About /> 
+          </div>
+          <div ref={(el) => componentRefs.current.push(el)}>
+            <Projects /> 
+          </div>
+          <div ref={(el) => componentRefs.current.push(el)}>
+            <Contact /> 
+          </div>
         </div>
         <div className="pages">
           {/* <Routes><Route path="/blog" element={<Blog />}/></Routes> */}
@@ -24,3 +45,4 @@ function App() {
 }
 
 export default App;
+
