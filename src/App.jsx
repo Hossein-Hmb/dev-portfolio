6428c5a7d8bf216dcc85b97b3265e2b7fa371c28
h.hajmirbaba <h.hajmirbaba@gmail.com>
2024-07-08 15:21:13 +0000

    push3

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
-import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
+import Projects from "./components/Projects.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="components">
-        <Routes>
-          <Route></Route>
-          <Route></Route>
-        </Routes>
-        <About />
-        <Projects />
-        <Contact />
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="components">
+          <About />
+          <Projects />
+          <Contact />
+          <Routes>
+            {/* <Route path="/blog" element={<Blog />}/> */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
